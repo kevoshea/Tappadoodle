c1a312506bf341a4ad905c40940c4f73f4933d44
Kevin O'Shea <kevinoshea@kevins-mac-mini.local>
2016-06-03 12:30:01 +0100

    Initial Commit

---
+++ Tappadoodle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tappadoodle
+//
+//  Created by Kevin O'Shea on 03/06/2016.
+//  Copyright © 2016 Kevin O'Shea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
